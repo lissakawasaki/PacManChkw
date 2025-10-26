79295d906da860b33ae4bf182c65bfb17962c3a5
Lissa Guirau <kawasakilissa@gmail.com>
2025-10-25 22:34:50 -0300

    reorganizando a estrutura

--- pm-cliente.py
+++ pm-cliente.py
@@ -16,17 +16,25 @@ pygame.display.set_icon(icon_image)
 def menu_principal():
     pygame.display.set_caption("Menu Principal")
     global running
+
+    titulo = pygame.image.load('sprites/titulo.png').convert_alpha()
+    jogar_img = pygame.image.load('sprites/bot_jogar.png').convert_alpha()
+    info_img = pygame.image.load('sprites/bot_info.png').convert_alpha()
+    sair_img = pygame.image.load('sprites/bot_sair.png').convert_alpha()
+    ajuda_img = pygame.image.load('sprites/bot_ajuda.png').convert_alpha()
+
+    bot_jogar = Button(145, 200, jogar_img)
+    bot_ajuda = Button(145, 300, ajuda_img)
+    bot_info = Button(145, 400, info_img)
+    bot_sair = Button(145, 500, sair_img)
+
     while running:
-                
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
         screen.fill((255, 250, 231))
-
         screen.blit(background, (0, 0))
-
-        titulo = pygame.image.load('sprites/titulo.png').convert_alpha()
         screen.blit(titulo, (80, 5))
 
         jogar_img = pygame.image.load('sprites/bot_jogar.png').convert_alpha()
@@ -54,22 +62,22 @@ def menu_principal():
 def info():
 
     pygame.display.set_caption("Informações")
-    
+
     info_running = True 
+
+    texto_info = pygame.image.load('sprites/mensagem_info.png').convert_alpha() 
+    voltar_img = pygame.image.load('sprites/bot_voltar.png').convert_alpha()
+    bot_voltar = Button(280, 530, voltar_img)
+
     while info_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 info_running = False
+                running = False
 
         screen.fill((255, 250, 231))
         screen.blit(background, (0, 0))
-
-        texto_info = pygame.image.load('sprites/mensagem_info.png').convert_alpha()
         screen.blit(texto_info, (0, 0))
-
-        voltar_img = pygame.image.load('sprites/bot_voltar.png').convert_alpha()
-
-        bot_voltar = Button(280, 530, voltar_img)
 
         if bot_voltar.draw(screen) == True:
             pygame.event.clear()
@@ -80,42 +88,39 @@ def info():
 
 def ajuda():
 
+    global running
     pygame.display.set_caption("Ajuda")
     
     ajuda_running = True 
+
+    comandos = pygame.image.load('sprites/comandos.png').convert_alpha()
+    voltar_img = pygame.image.load('sprites/bot_voltar.png').convert_alpha()
+    bot_voltar = Button(280, 530, voltar_img)
+    
+    texto_setas1 = fonte.render("Use as setas", True, (88, 27, 141)) 
+    texto_setas2 = fonte.render("para mover-se.", True, (88, 27, 141)) 
+    texto_p1 = fonte.render("Pressione 'P'", True, (88, 27, 141)) 
+    texto_p2 = fonte.render("para pausar.", True, (88, 27, 141)) 
+    texto_s1 = fonte.render("Pressione 'S'", True, (88, 27, 141)) 
+    texto_s2 = fonte.render("para sair.", True, (88, 27, 141)) 
+
     while ajuda_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 ajuda_running = False
+                running = False
 
         screen.fill((255, 250, 231))
         screen.blit(background, (0, 0))
-
-        comandos = pygame.image.load('sprites/comandos.png').convert_alpha()
         screen.blit(comandos, (60, 60))
         
-        texto_setas1 = fonte.render("Use as setas", True, (88, 27, 141)) 
-        texto_setas2 = fonte.render("para mover-se.", True, (88, 27, 141)) 
-     
         screen.blit(texto_setas1, (300, 100)) 
         screen.blit(texto_setas2, (300, 130)) 
-
-        texto_p1 = fonte.render("Pressione 'P'", True, (88, 27, 141)) 
-        texto_p2 = fonte.render("para pausar.", True, (88, 27, 141)) 
-
         screen.blit(texto_p1, (300, 270)) 
         screen.blit(texto_p2, (300, 300)) 
-
-        texto_s1 = fonte.render("Pressione 'S'", True, (88, 27, 141)) 
-        texto_s2 = fonte.render("para sair.", True, (88, 27, 141)) 
-
         screen.blit(texto_s1, (300, 410)) 
-        screen.blit(texto_s2, (300, 440)) 
-
-        voltar_img = pygame.image.load('sprites/bot_voltar.png').convert_alpha()
-
-        bot_voltar = Button(280, 530, voltar_img)
-
+        screen.blit(texto_s2, (300, 440))
+        
         if bot_voltar.draw(screen) == True:
             pygame.event.clear()
             pygame.time.delay(100)
